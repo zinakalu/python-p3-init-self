e42a1aac31bc2f4c40c0efffc11dabdabd139c6d
zinakalu <zina.kalu@gmail.com>
2023-04-05 05:42:45 -0400

    init

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+
+bingo = Dog("Bingo")
+bingo.name
+#Bingo
